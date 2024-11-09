f19d3e08aae742a31ed420431042030307fa47ba
Soup of the tomato kind <43444191+Soup-64@users.noreply.github.com>
2024-11-09 17:46:01 -0500

    Update 244850.py
    first edit for re adding dotnet fixes, couple reasons why it's needed again
    - no longer requires dotnet 5
    - applies much more consistently
    - wine-mono support on SE has only declined, dotnet is a must have for using the steam workshop, multiplayer, and often for performance.
    Only risk is sometimes winetricks breaks the dotnet verb and the install fails to drop core dlls like mscoree.dll.

--- gamefixes-steam/244850.py
+++ gamefixes-steam/244850.py
@@ -5,6 +5,10 @@ from protonfixes import util
 
 def main() -> None:
     util.protontricks('xaudio29')
+    check = util.protontricks('dotnet48')
+
+    if(!check):
+        try_show_gui_error("dotnet failed to install, the prefix is likely broken now.")
 
     base_attibutte = '<runtime>'
     game_opts = """
